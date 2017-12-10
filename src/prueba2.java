454c317a25f63f1b869e2f9c9a6f7c590b5c6c02
denber <git@github.com:denber/prueba1.git>
2017-12-10 01:41:43 -0500

    hola

---
+++ src/prueba2.java
@@ -0,0 +1,10 @@
+/**
+ * Created by denbe on 09/12/2017.
+ */
+public class prueba2 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hola mundillo");
+        System.out.println("FGFFGHH");
+    }
+}
