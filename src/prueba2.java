74b93b943fe5fd1c902733cc94a2f93a7f39fb7b
denber <git@github.com:denber/prueba1.git>
2017-12-10 02:02:12 -0500

    hola

--- src/prueba2.java
+++ src/prueba2.java
@@ -5,6 +5,6 @@ public class prueba2 {
     public static void main(String[] args)
     {
         System.out.println("Hola mundillo");
-        System.out.println("FGFFGHH");
+        System.out.println("FGFFfgfdGHH");
     }
 }
